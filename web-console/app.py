2f938cd1535c84cddf759caaedb509baca37ef68
SirJBiscuit <jcompute256@gmail.com>
2026-05-11 14:10:18 -0400

    Fix Origin header format - use list instead of dict

--- web-console/app.py
+++ web-console/app.py
@@ -655,15 +655,14 @@ def connect_to_pterodactyl_console(server_id, ws_url, ws_token, client_sid):
     
     try:
         # Create WebSocket connection with Origin header
+        # websocket-client expects headers as a list of 'Key: Value' strings
         ws = websocket.WebSocketApp(
             ws_url,
             on_open=on_open,
             on_message=on_message,
             on_error=on_error,
             on_close=on_close,
-            header={
-                'Origin': 'https://console.cloudmc.online'
-            }
+            header=['Origin: https://console.cloudmc.online']
         )
         
         # Store the connection
